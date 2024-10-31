b4179620dbece1e9a2c47fa86a726f001d50723d
Dariill <darksous33@gmail.com>
2024-10-31 13:31:18 +0300

    variables_31_10.py

---
+++ variables_31_10.py
@@ -0,0 +1,13 @@
+
+
+completed = 12
+hours = 1.5
+course_name = 'Python'
+time_per_task = (hours/completed)
+print (time_per_task)
+
+print ('Курс:',course_name + ', количество задач:', completed , ', затраченное время:',hours,  ", среднее время выполнения:" ,time_per_task ,"часа.")
+
+
+
+
